205ca357ea517687e2e0c3290cae8e196ced0b65
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-01-25 21:29:00 +0530

    structures categories list

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:meals_pro/dummy_data.dart';
+import 'package:meals_pro/widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200, childAspectRatio: 3 / 2),
-      children: [],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Meals Pro',
+          style: TextStyle(
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: GridView(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 350,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 30),
+        children: [
+          ...DUMMY_CATEGORIES.map(
+            (catData) => CategoryItem(catData.title, catData.bgColor),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
-  final String bgColor;
+  final Color bgColor;
 
   const CategoryItem(this.title, this.bgColor, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [bgColor.withOpacity(0.7), bgColor],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Text(title),
+    );
   }
 }
