ee42e250de203ada0583d2fde075e61a991df9b1
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-01-25 18:10:33 +0530

    setup category list

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final String bgColor;
+
+  const CategoryItem(this.title, this.bgColor, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
