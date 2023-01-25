08f0679fa4f5fa3a04cc7a30c9b314f5169d084a
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-01-25 21:28:21 +0530

    categoryscreen as home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_pro/screens/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,30 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meals Pro',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meals Pro'),
-      ),
-      body: Container(),
-    );
+        title: 'Meals Pro',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: CategoriesScreen());
   }
 }
