779c5c13389702914a67237c34366c9e38a41721
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-01-25 18:10:16 +0530

    add base data

---
+++ lib/dummy_data.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+// ignore: constant_identifier_names
+const DUMMY_CATEGORIES = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    bgColor: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    bgColor: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    bgColor: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    bgColor: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    bgColor: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    bgColor: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    bgColor: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    bgColor: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    bgColor: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    bgColor: Colors.teal,
+  ),
+];
