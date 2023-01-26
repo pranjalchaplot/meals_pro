f0b8b42ef2eafefa7164a20b9757989d9e1fe813
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-01-26 22:49:32 +0530

    add material page route

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
+import '../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color bgColor;
 
-  const CategoryItem(this.title, this.bgColor, {super.key});
+  const CategoryItem(this.id, this.title, this.bgColor, {super.key});
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsScreen(id, title, bgColor);
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [bgColor.withOpacity(0.7), bgColor],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [bgColor.withOpacity(0.7), bgColor],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
       ),
-      child: Text(title),
     );
   }
 }
