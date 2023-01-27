a9e3b6a4b37ddfdc5d65d5c3825a363c5bd7e048
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-01-27 21:52:25 +0530

    appbar theming

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({super.key});
+  final String categoryTitle;
+  final String categoryId;
+  final Color bgColor;
+  const CategoryMealsScreen(this.categoryId, this.categoryTitle, this.bgColor,
+      {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('recipes app bar, lol.'),
+        title: Text(categoryTitle),
+        // backgroundColor: bgColor,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [bgColor.withOpacity(0.8), bgColor],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('The \'Pro\' Recipie'),
+      body: Center(
+        child: Text('The Recipie of $categoryTitle dishes'),
       ),
     );
   }
