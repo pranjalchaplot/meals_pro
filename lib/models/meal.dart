006102aed1f740aa923e0fc3cc325b0799935dbc
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-02-03 15:18:15 +0530

    add meal model

---
+++ lib/models/meal.dart
@@ -0,0 +1,43 @@
+enum Complexity {
+  simple,
+  challenging,
+  hard,
+}
+
+enum Affordability {
+  affordable,
+  pricey,
+  luxurious,
+}
+
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imgUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+  final bool isGluttenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+
+  const Meal({
+    required this.id,
+    required this.categories,
+    required this.title,
+    required this.imgUrl,
+    required this.affordability,
+    required this.complexity,
+    required this.duration,
+    required this.ingredients,
+    required this.isGluttenFree,
+    required this.isLactoseFree,
+    required this.isVegan,
+    required this.isVegetarian,
+    required this.steps,
+  });
+}
